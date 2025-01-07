9af335a5b9c78b5ce973a8df3115d2e0162371c5
Marlon <marloninfanteramirez@yahoo.com.ar>
2025-01-07 00:47:28 -0300

    Endpoints create, update, delete, getProduct and getProducts have been created

--- backend/src/controllers/product.ts
+++ backend/src/controllers/product.ts
@@ -1,43 +1,94 @@
-import { Response, Request } from "express";
+import { Response, Request, NextFunction } from "express";
+import Product from "../models/product";
 
-export const getProducts= (req: Request, res: Response)=>{
-    res.json({
-        msg: 'Get products'       
-    });
+export const getProducts = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const listProducts = await Product.findAll();
+        console.log(listProducts);
+        res.json(listProducts);
+    } catch (error) {
+        next(error);
+    }
+
 };
 
-export const getProduct= (req: Request, res: Response)=>{
-    const {id}= req.params;
-    res.json({
+export const getProduct = async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+    try {
+        const product = await Product.findByPk(id);
+        if (product) {
+            res.json(product);
+        } else {
+            res.status(404).json({
+                message: `There is not product by id : ${id}`
+            })
+        }
+    } catch (error) {
+        next(error);
+    }
+
+    /*res.json({
         msg: 'Get product',
-        id     
-    });
+        id
+    });*/
+
 };
 
-export const deleteProduct= (req: Request, res: Response)=>{
-    const {id}= req.params;
-    res.json({
-        msg: 'delete product',
-        id     
-    });
+export const deleteProduct = async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+    try {
+        const product = await Product.findByPk(id);
+        if (!product) {
+            res.status(404).json({
+                message: `There is not product by id : ${id}`
+            })
+        } else {
+            await product.destroy();
+            res.json({
+                message: `The product was eliminated`
+            })
+        }
+    } catch (error) {
+        next(error);
+    }
 };
 
-export const postProduct= (req: Request, res: Response)=>{
-    const {body}= req;
-    console.log(body);
-    res.json({
-        msg: 'Post product',
-        body : body    
-    });
+export const postProduct = async (req: Request, res: Response) => {
+    const { body } = req;
+    try {
+        await Product.create(body);
+        res.json({
+            message: 'Product created'
+        });
+    } catch (error) {
+        res.json({
+            message: 'It has occurred an error. Please, you contact to support'
+        });
+    }
+
 };
 
-export const updateProduct= (req: Request, res: Response)=>{
-    const {body}= req;
-    const {id}= req.params;
-    console.log(body);
-    res.json({
-        msg: 'Post product',
-        id,
-        body
-    });
+export const updateProduct = async(req: Request, res: Response) => {
+    const { body } = req;
+    const { id } = req.params;
+    try {
+        const product= await Product.findByPk(id);
+        if (product) {
+            await product.update(body);
+            res.json({
+                message:'The product was update'
+            })
+        } else {
+            res.status(404).json({
+                message: `There is not product by id: ${id}`
+            })
+        }
+    } catch (error) {
+        res.json({
+            message: 'It has occurred an error. Please, you contact to support'
+        });
+    }
 };
+
+//PARECE SER QUE HAY QUE PONER EL NEXT EN TODOS LOS METODOS. GETPRODUCT Y GERPRODUCTS AHORA FUNCIONAN
+// BIEN PERO DELETE NO SI SOLICITO BORRAR DOS VECES CON EL MISMO ID A LA SEGUNDA VEZ APARECE EL ERROR
--- backend/src/models/server.ts
+++ backend/src/models/server.ts
@@ -1,37 +1,52 @@
 import express, { Application, Request, Response } from 'express';
 import routeProduct from '../routes/product';
+import db from '../db/connection';
 
-class Server{
+class Server {
 
     private app: Application;
     private port: string;
 
-    constructor(){
-        this.app= express();
-        this.port= process.env.PORT || '3001' ;
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT || '3001';
         this.listen();
         //esta funcion debe ser invocada antes de los routes para que funcione:
         this.middleware();
         this.routes();
+        this.dbConnect();
     }
 
-    listen(){
-        this.app.listen(this.port, ()=>{
+    listen() {
+        this.app.listen(this.port, () => {
             console.log(`The application is running on port ${this.port}`);
         })
     }
 
-    routes(){
-        this.app.get('/', (req: Request, res: Response )=>{
+    routes() {
+        this.app.get('/', (req: Request, res: Response) => {
             res.json({
                 message: 'API is working'
             });
         })
+
         this.app.use('/api/products', routeProduct);
     }
 
-    middleware(){
+    middleware() {
         this.app.use(express.json());
+    }
+
+    async dbConnect() {
+        try {
+            await db.authenticate();
+            console.log('Database is connecting');
+        } catch (error) {
+            console.log(error);
+            console.log('Error connecting to database');
+        }
+        
+
     }
 }
 
